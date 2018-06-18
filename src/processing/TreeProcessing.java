17e69e87016f41a7fdeb23094c09f19bd3f03804
Flip <itsvizu@gmail.com>
2018-06-18 15:06:20 +0200

    oopsie

--- src/processing/TreeProcessing.java
+++ src/processing/TreeProcessing.java
@@ -130,7 +130,7 @@ public final class TreeProcessing {
 			IDDeclarationBlock domain = new IDDeclarationBlock();
 			tree.root.checkTypes(domain, Scope.GLOBAL);
 			tree.nrOfGlobals = domain.globalVars.size();
-			domain.findFunDeclaration("main");
+			tree.mainAddress = domain.findFunDeclaration("main").branchAddress;
 		} catch (TypeException e) {
 			e.printStackTrace();
 			return false;
